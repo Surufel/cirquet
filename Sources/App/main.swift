8a2af24bb071a14160a207d1eca033dbfebf5e8d
Kurt Bitner <kurt@bitner.co>
2016-11-04 14:28:15 -0400

    test commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,8 +10,33 @@ drop.get { req in
 
 drop.get("hi") {
     request in
-    return try drop.view.make("hi.html");
+    return try drop.view.make("hi.html")
 }
-drop.resource("posts", PostController())
+
+//drop.resource("posts", PostController())
+
+drop.socket("ws") { req, ws in
+    print("New WebSocket connected: \(ws)")
+    
+    // ping the socket to keep it open
+    try background {
+        while ws.state == .open {
+            try? ws.ping()
+            drop.console.wait(seconds: 10) // every 10 seconds
+        }
+    }
+    
+    ws.onText = { ws, text in
+        print("Text received: \(text)")
+        
+        // reverse the characters and send back
+        let rev = String(text.characters.reversed())
+        try ws.send(rev.bytes)
+    }
+    
+    ws.onClose = { ws, code, reason, clean in
+        print("Closed.")
+    }
+}
 
 drop.run()
