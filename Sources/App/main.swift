375b8c9f712893eb16e7a4c7b333e4430eb7ee6a
Kurt Bitner <kurt@bitner.co>
2016-11-12 17:26:44 -0500

    ws1

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -34,25 +34,8 @@ drop.get ("12345") {
 drop.socket("ws") { req, ws in
     print("New WebSocket connected: \(ws)")
     
-    // ping the socket to keep it open
-    try background {
-        while ws.state == .open {
-            try? ws.ping()
-            drop.console.wait(seconds: 10) // every 10 seconds
-        }
-    }
+    try ws.send("connected");
     
-    ws.onText = { ws, text in
-        print("Text received: \(text)")
-        
-        // reverse the characters and send back
-        let rev = String(text.characters.reversed())
-        try ws.send(rev)
-    }
-    
-    ws.onClose = { ws, code, reason, clean in
-        print("Closed.")
-    }
 }
 
 
