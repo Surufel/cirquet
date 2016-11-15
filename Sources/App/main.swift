bb3263d8c014f27960eb67271d2064e14f9e5ec3
Kurt Bitner <kurt@bitner.co>
2016-11-15 15:47:02 -0500

    psql

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,10 +1,10 @@
 import Vapor
 //probably import chat file
-//import VaporPostgreSQL
+import VaporPostgreSQL
 
 
 let drop = Droplet()
-//try drop.addProvider(VaporPostgreSQL.Provider.self);
+try drop.addProvider(VaporPostgreSQL.Provider.self);
 
 drop.get { req in
     return try drop.view.make("welcome", [
