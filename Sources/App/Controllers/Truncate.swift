be7f6de8aba532c1966b1dbfb70312b3564a671d
Kurt Bitner <kurt@bitner.co>
2016-11-19 16:50:52 -0500

    string truncate

---
+++ Sources/App/Controllers/Truncate.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension String {
+    func truncated(to max: Int) -> String {
+        if characters.count > max {
+            return substring(to: index(startIndex, offsetBy: max))
+        }
+        return self
+    }
+}
